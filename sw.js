cf6863547c88d8ecbec4116951350aeee4042fb7
Your GitHub Username <your-github-email@example.com>
2025-11-05 19:20:25 +0800

    feat: Time Bank 4.3.1 版本回退

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// sw.js - v4.3.5 Compatible Version
+// sw.js - v4.3.1 Compatible Version
 
-// [v4.3.5] 更新缓存名称，修复 v4.3.4 崩溃 Bug
-const CACHE_NAME = 'timebank-v4.3.5'; 
+// [v4.3.1] 更新缓存名称 (Fix: 修复 habit rebuild 时区 Bug)
+const CACHE_NAME = 'timebank-v4.3.1'; 
 const urlsToCache = [
   '/time-bank/',
   '/time-bank/index.html',
