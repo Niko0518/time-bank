4e5a21ae0df6fd1c04da3ba98937ba53a5876728
Your GitHub Username <your-github-email@example.com>
2025-12-24 15:33:29 +0800

    5.5.1: 修复云同步LiveQuery丢失+补记习惯奖励+修复工具

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Time Bank Service Worker - v5.5.0
-const CACHE_NAME = 'timebank-cache-v5.5.0';
+// Time Bank Service Worker - v5.5.1
+const CACHE_NAME = 'timebank-cache-v5.5.1';
 const ASSETS = [
     './',
     './index.html',
