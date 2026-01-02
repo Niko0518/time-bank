dc3ce52a14efbcedc55b56332dff6ef085193b50
Your GitHub Username <your-github-email@example.com>
2026-01-02 17:14:35 +0800

    5.8.1 全面更新同步机制

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿// Time Bank Service Worker - v5.8.0
-const CACHE_NAME = 'timebank-cache-v5.8.0';
+﻿// Time Bank Service Worker - v5.8.1
+const CACHE_NAME = 'timebank-cache-v5.8.1';
 const ASSETS = [
     './',
     './index.html',
