aca04179cacd72233ffacae3fdc552788038e209
Your GitHub Username <your-github-email@example.com>
2026-01-17 16:34:17 +0800

    v7.2.0: 分类栏排序功能

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Time Bank Service Worker - v7.1.8
-const CACHE_NAME = 'timebank-cache-v7.1.8';
+// Time Bank Service Worker - v7.2.0
+const CACHE_NAME = 'timebank-cache-v7.2.0';
 const ASSETS = [
     './',
     './index.html',
